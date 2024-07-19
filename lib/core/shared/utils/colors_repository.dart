bb96e42883a9978ca59c455f47c733faf7b42304
cafoga98 <camiloforerogalindo@gmail.com>
2024-07-19 14:44:02 -0500

    Add color repository and image repository

---
+++ lib/core/shared/utils/colors_repository.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/animation.dart';
+
+class ColorsRepository {
+  ColorsRepository._();
+
+  ///Principal color
+  static const realBlue = Color(0xFF3761A8);
+  static const goldenPoppy = Color(0xFFFECA1A);
+  static const platinum = Color(0xFFE5E5E5);
+  static const coralRed = Color(0xFFFF4136);
+  static const limeGreen = Color(0xFF2ECC40);
+}
