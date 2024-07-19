45786cadf0c264ddf3cfd3d885d7218b42f8a94a
cafoga98 <camiloforerogalindo@gmail.com>
2024-07-19 16:30:50 -0500

    Add bloc to login page

---
+++ lib/features/login/domain/bloc/login_bloc/login_bloc.dart
@@ -0,0 +1,51 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:poke_app/core/data/repository/implementation/local_storage_repository.dart';
+
+import '/core/shared/error_handle/failure.dart';
+
+part 'login_event.dart';
+
+part 'login_state.dart';
+
+part 'login_bloc.freezed.dart';
+
+/// A bloc that manages the login state of an app.
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final LocalStorageRepository localStorageRepository;
+
+  /// Creates a new LoginBloc instance.
+  LoginBloc({
+    required this.localStorageRepository,
+  }) : super(const LoginState.initial()) {
+    on<LoginEvent>(_fetchLogin);
+  }
+
+  /// Fetches the login state from the login repository.
+  void _fetchLogin(LoginEvent event, Emitter<LoginState> emit) async {
+    /// Emit a loading state to indicate that the login process is in progress.
+    emit(const LoginState.loading());
+
+    /// Wait for 2 seconds to simulate a network request.
+    await Future.delayed(const Duration(seconds: 2));
+
+    /// Call the login repository to login the user.
+    final response = await localStorageRepository.getLocalUser();
+
+    /// Emit the appropriate state depending on the response from the login repository.
+    emit(
+      response.fold(
+        (left) {
+          return LoginState.error(failure: left);
+        },
+        (right) {
+          if (right.username == event.userName &&
+              right.password == event.password) {
+            return LoginState.done(message: 'Login Correcto');
+          }
+          return LoginState.done(message: 'Login Incorrecto');
+        },
+      ),
+    );
+  }
+}
