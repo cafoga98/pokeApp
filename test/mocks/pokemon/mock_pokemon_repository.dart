d4199c3d56cab2843a2ec87bfd1c76d2783f97e4
cafoga98 <camiloforerogalindo@gmail.com>
2024-07-20 12:13:46 -0500

    Create mocks to make test

---
+++ test/mocks/pokemon/mock_pokemon_repository.dart
@@ -0,0 +1,4 @@
+import 'package:mockito/annotations.dart';
+
+@GenerateNiceMocks([MockSpec<PokemonRepository>()])
+import 'package:poke_app/features/pokemon_list/data/repositories/implementation/pokemon_repository.dart';
