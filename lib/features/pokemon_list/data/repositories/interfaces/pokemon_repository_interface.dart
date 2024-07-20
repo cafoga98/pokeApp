41e6bfc2250942a02661053f62a4d445873e80f5
cafoga98 <camiloforerogalindo@gmail.com>
2024-07-19 20:56:05 -0500

    Add repository and service to pokemon feature

---
+++ lib/features/pokemon_list/data/repositories/interfaces/pokemon_repository_interface.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:poke_app/features/pokemon_list/domain/entities/pokemon.dart';
+import 'package:poke_app/features/pokemon_list/domain/entities/pokemon_detail.dart';
+
+import '/core/shared/error_handle/failure.dart';
+
+abstract class PokemonRepositoryInterfaces {
+  Future<Either<Failure, List<Pokemon>>> fetchPokemons({required int page});
+
+  Future<Either<Failure, PokemonDetail>> fetchPokemonDetail(
+      {required String url});
+}
