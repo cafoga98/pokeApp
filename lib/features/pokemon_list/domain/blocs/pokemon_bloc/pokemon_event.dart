d5dc25b834127e190270ce63cdaa1b03e6e8c3e9
cafoga98 <camiloforerogalindo@gmail.com>
2024-07-20 10:10:09 -0500

    Add new api consume to search pokemon

--- lib/features/pokemon_list/domain/blocs/pokemon_bloc/pokemon_event.dart
+++ lib/features/pokemon_list/domain/blocs/pokemon_bloc/pokemon_event.dart
@@ -6,4 +6,5 @@ class PokemonEvent with _$PokemonEvent {
   const factory PokemonEvent.started() = Started;
   const factory PokemonEvent.fetchPokemons({required HandlePage handlePage}) = FetchPokemons;
   const factory PokemonEvent.fetchPokemonDetail({required String url}) = FetchPokemonDetail;
+  const factory PokemonEvent.searchPokemons({required String name}) = SearchPokemons;
 }
